8244b70dcb46c5cdf91f078be7f1098d470f5b44
Dale Wijnand <dale.wijnand@gmail.com>
2016-04-21 23:49:53 +0100

    Ultimately, align impl only, two groups

--- src/main/scala/str/Str.scala
+++ src/main/scala/str/Str.scala
@@ -5,26 +5,23 @@ import scala.{ PartialFunction => ?=> }
 final case class Str(private val value: String) extends AnyVal {
   import Str.empty
 
-  @inline final def  isEmpty: Boolean =  value.isEmpty
+  @inline final def unsafeGet: String = value
+  @inline final def isEmpty: Boolean  = value.isEmpty
   @inline final def nonEmpty: Boolean = !value.isEmpty
 
-  @inline final def contains(s: String)           : Boolean = nonEmpty && value == s
-  @inline final def   exists(p: String => Boolean): Boolean = nonEmpty && p(value)
-  @inline final def   forall(p: String => Boolean): Boolean =  isEmpty || p(value)
-
-  @inline final def getOrElse(alt: => String): String      = if (isEmpty) alt else value
-  @inline final def    orElse(alt: => Str           ): Str = if (isEmpty) alt else this
-  @inline final def       map( f: String  => String ): Str = if (isEmpty) this else Str(f(value))
-  @inline final def   flatMap( f: String  => Str    ): Str = if (isEmpty) this else f(value)
-  @inline final def filter   ( p: String  => Boolean): Str = if (isEmpty ||  p(value)) this else empty
-  @inline final def filterNot( p: String  => Boolean): Str = if (isEmpty || !p(value)) this else empty
-  @inline final def   collect(pf: String ?=> String ): Str = if (isEmpty) this else Str(pf.applyOrElse(value, (_: String) => ""))
-  @inline final def fold[A](alt: => A)(f: String => A): A  = if (isEmpty) alt else f(value)
-  @inline final def cata[A](f: String => A, alt: => A): A  = if (isEmpty) alt else f(value)
-
-  @inline final def unsafeGet: String = value
-
-  @inline final def foreach(f: String => Unit): Unit = if (nonEmpty) f(value)
+  @inline final def contains(s: String): Boolean          = nonEmpty && value == s
+  @inline final def exists(p: String => Boolean): Boolean = nonEmpty && p(value)
+  @inline final def forall(p: String => Boolean): Boolean = isEmpty || p(value)
+  @inline final def getOrElse(alt: => String): String     = if (isEmpty) alt else value
+  @inline final def orElse(alt: => Str): Str              = if (isEmpty) alt else this
+  @inline final def map(f: String => String): Str         = if (isEmpty) this else Str(f(value))
+  @inline final def flatMap(f: String => Str): Str        = if (isEmpty) this else f(value)
+  @inline final def filter(p: String => Boolean): Str     = if (isEmpty || p(value)) this else empty
+  @inline final def filterNot(p: String => Boolean): Str  = if (isEmpty || !p(value)) this else empty
+  @inline final def collect(pf: String ?=> String): Str   = if (isEmpty) this else Str(pf.applyOrElse(value, (_: String) => ""))
+  @inline final def fold[A](alt: => A)(f: String => A): A = if (isEmpty) alt else f(value)
+  @inline final def cata[A](f: String => A, alt: => A): A = if (isEmpty) alt else f(value)
+  @inline final def foreach(f: String => Unit): Unit      = if (nonEmpty) f(value)
 
   @inline override final def toString = value
 }
