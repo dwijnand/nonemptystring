bfac70677f3860c3e1092da27a85e2be7160cfcb
Dale Wijnand <dale.wijnand@gmail.com>
2016-05-03 11:49:38 +0100

    Document NonEmptyString and Str

--- src/main/scala/str/NonEmptyString.scala
+++ src/main/scala/str/NonEmptyString.scala
@@ -6,6 +6,22 @@ import scala.{ Any, AnyVal, Boolean, IllegalArgumentException, None, Option, Som
 import scala.collection.immutable.Seq
 import scala.reflect.macros.{ TypecheckException, blackbox }
 
+/** `NonEmptyString` is a type that removes the empty string from the possible values of `String`. This is done
+  * by guaranteeing it on construction, via one of the methods on the companion object or the `nes` string
+  * interpolator.
+  *
+  * Because all non-empty strings are still strings, an unlift implicit is present so that string-like methods
+  * are available.
+  *
+  * The implementation is a value class which means, within the limits of value classes, there should be no
+  * runtime cost.
+  *
+  * The implementation is not a case class because you can't overload case class companion object apply and also
+  * case classes have copy methods that could break the non-empty guarantee. However, given the implementation is
+  * a value class, hashCode and equals are correctly defined in terms of the underlying value.
+  *
+  * @param value the underlying string value
+  */
 final class NonEmptyString private (val value: String) extends AnyVal {
   @inline override def toString = value
 }
--- src/main/scala/str/Str.scala
+++ src/main/scala/str/Str.scala
@@ -3,6 +3,17 @@ package str
 import java.lang.String
 import scala.{ AnyVal, Boolean, inline }
 
+/** `Str` represents a coproduct of the empty string and non-empty strings, forcing you to reason about the
+  * behaviour of your code in the presence of an empty string.
+  *
+  * For this reason it doesn't provide typical string-like methods, nor does it provide an implicit to unlift it
+  * back to String.
+  *
+  * The implementation is a value class which means, within the limits of value classes, there should be no
+  * runtime cost.
+  *
+  * @param value the underlying string value
+  */
 final case class Str(value: String) extends AnyVal {
   @inline final def isEmpty: Boolean  = value.isEmpty
   @inline final def nonEmpty: Boolean = !value.isEmpty
